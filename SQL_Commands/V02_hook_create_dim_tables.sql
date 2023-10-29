3adae2a0db1b39ce23e5146bff3f9390a5ed22f1
Rihab Atwi <rihab112000@gamil.com>
2023-10-29 02:55:20 +0200

    Edited checkin dim

--- SQL_Commands/V02_hook_create_dim_tables.sql
+++ SQL_Commands/V02_hook_create_dim_tables.sql
@@ -239,14 +239,17 @@ DO UPDATE SET
 CREATE TABLE IF NOT EXISTS target_schema.dim_checkin
 (
     business_id TEXT,
-    checkin_time TIMESTAMP 
+    checkin_time TIMESTAMP ,
+    id INTEGER PRIMARY KEY NOT NULL
 );
-CREATE INDEX IF NOT EXISTS "idx_business_id" ON target_schema.dim_checkin(business_id);
-INSERT INTO target_schema.dim_checkin (business_id, checkin_time)
+CREATE INDEX IF NOT EXISTS "idx_id" ON target_schema.dim_checkin(id);
+INSERT INTO target_schema.dim_checkin (business_id, checkin_time,id)
 SELECT DISTINCT
     src_table.business_id, 
-    src_table.date
-FROM target_schema.stg_checkin as src_table;
-ON CONFLICT (business_id, checkin_time) 
+    src_table.date,
+    src_table.id
+FROM target_schema.stg_checkin as src_table
+ON CONFLICT(id) 
 DO UPDATE SET 
-    checkin_time = EXCLUDED.checkin_time;
+    checkin_time = EXCLUDED.checkin_time,
+    business_id = EXCLUDED.business_id;
