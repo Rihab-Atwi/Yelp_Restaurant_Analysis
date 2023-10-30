aabc002c8ac0da5bba3c94f1febeed9632a3b748
Rihab Atwi <rihab112000@gamil.com>
2023-10-30 16:28:19 +0200

    Added loggers

--- posthook.py
+++ posthook.py
@@ -12,7 +12,6 @@ def truncate_staging_table(db_session, source_name=DestinationSchemaName.Datawar
                 TRUNCATE TABLE {dst_table}
             """
             execute_query(db_session, truncate_query)
-            print(f"Table {dst_table} truncated successfully.")
         tables_names = [member.value for member in StagingTablesNamesWithDate]
         for table_name in tables_names:
             dst_table = f"{source_name.value}.{table_name}"
@@ -20,14 +19,24 @@ def truncate_staging_table(db_session, source_name=DestinationSchemaName.Datawar
                 TRUNCATE TABLE {dst_table}
             """
             execute_query(db_session, truncate_query)
-            print(f"Table {dst_table} truncated successfully.")
     except Exception as e:
         show_error_message(ErrorHandling.TRUNCATE_ERROR.value, str(e))
 
-def execute_posthook():
+def execute_posthook(logger):
+    step = None
+    logger.info("Posthook:")
     try:
+        step = 1
+        logger.info("Step 1: Create a database connection")
         db_session = create_connection()
+
+        step=2
+        logger.info("Step 2: Truncate staging table")
         truncate_staging_table(db_session, source_name = DestinationSchemaName.Datawarehouse)
+
+        step=3
+        logger.info("Step 3: Close database connection")
         close_connection(db_session)
+
     except Exception as e:
         show_error_message(ErrorHandling.EXECUTE_POSTHOOK_ERROR.value, str(e))
